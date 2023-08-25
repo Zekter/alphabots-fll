7f2b64d3b27c8e4d8fc0007e9c8a371296febe02
Zekter <zekter598@gmail.com>
2023-08-25 13:00:56 +0400

    made a team page and made changes to the journey page

--- journey.js
+++ journey.js
@@ -33,4 +33,30 @@ DecB.addEventListener("click", () => {
 
 
 
+// Select the button element
+let mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 1000 || document.documentElement.scrollTop > 1000) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document smoothly
+function topFunction() {
+  // Use behavior: "smooth" to achieve a smooth scroll effect
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  });
+}
+
+
 
